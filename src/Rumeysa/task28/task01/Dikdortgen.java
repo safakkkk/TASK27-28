380248d89c5b6c34a780bf53babceb2772830439
Rumeysa <rumeysaarsvn@gmail.com>
2023-08-04 21:47:15 +0200

    task added

--- src/Rumeysa/task28/task01/Dikdortgen.java
+++ src/Rumeysa/task28/task01/Dikdortgen.java
@@ -1,7 +1,22 @@
 package Rumeysa.task28.task01;
 
-public class Dikdortgen{
+public class Dikdortgen implements Sekil{
 
+    private int uzunKenar;
+    private int kisaKenar;
 
+    public Dikdortgen(int uzunKenar, int kisaKenar) {
+        this.uzunKenar = uzunKenar;
+        this.kisaKenar = kisaKenar;
+    }
 
+    @Override
+    public int cevre(int... boyut) {
+        return (2 * (kisaKenar + uzunKenar));
+    }
+
+    @Override
+    public int alan(int... boyut) {
+        return kisaKenar * uzunKenar;
+    }
 }
